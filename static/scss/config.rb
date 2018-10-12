7549f925adbd39261ee1d0acef09a2ef53c51a93
Hussain Shahbaz Khawaja <hskhawaja@live.com>
2018-10-12 11:09:53 +0500

    Add files via upload

---
+++ static/scss/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+        css_dir = "../css"
+        sass_dir = "."
+        images_dir = "../img"
+        javascripts_dir = "../js"
+        output_style = :nested
+        relative_assets=true
+        line_comments = false
+        
